061d52ac242e51f9b696e13b71abf19051c6a2b0
Zormorych <zormorych@gmail.com>
2026-02-27 09:48:27 +0100

    TaskTitle record added

--- src/main/java/com/pdropalazn/taskforge/tasks/domain/model/vo/TaskTitle.java
+++ src/main/java/com/pdropalazn/taskforge/tasks/domain/model/vo/TaskTitle.java
@@ -1,4 +1,15 @@
 package com.pdropalazn.taskforge.tasks.domain.model.vo;
 
-public record TaskTitle() {
+public record TaskTitle(String name) {
+
+    public TaskTitle {
+        if (name == null){
+            throw new IllegalArgumentException("Name can't be empty or null");
+        }
+        name = name.trim();
+        if (name.isEmpty()) throw new IllegalArgumentException("Name can't be empty or null");
+        if (name.length() < 3) throw new IllegalArgumentException("Name length must be at least 3 characters");
+        if (name.length() > 200) throw new IllegalArgumentException("Name length cannot be longer than 200 characters");
+    }
+
 }
