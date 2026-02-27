7ece22db786e7702ba898508a8e2e63bc376c9cf
Zormorych <zormorych@gmail.com>
2026-02-27 10:01:05 +0100

    TaskDecription added

--- src/main/java/com/pdropalazn/taskforge/tasks/domain/model/vo/TaskDescription.java
+++ src/main/java/com/pdropalazn/taskforge/tasks/domain/model/vo/TaskDescription.java
@@ -1,4 +1,13 @@
 package com.pdropalazn.taskforge.tasks.domain.model.vo;
 
-public record TaskDescription() {
+public record TaskDescription(String description) {
+
+    public TaskDescription {
+        description = description.trim();
+
+        if (description.length() > 1000){
+            throw new IllegalArgumentException("Description cannot be longer than 1000 characters");
+        }
+
+    }
 }
--- src/main/java/com/pdropalazn/taskforge/tasks/domain/model/vo/TaskTitle.java
+++ src/main/java/com/pdropalazn/taskforge/tasks/domain/model/vo/TaskTitle.java
@@ -1,15 +1,17 @@
 package com.pdropalazn.taskforge.tasks.domain.model.vo;
 
-public record TaskTitle(String name) {
+public record TaskTitle(String title) {
 
     public TaskTitle {
-        if (name == null){
+        if (title == null){
             throw new IllegalArgumentException("Name can't be empty or null");
         }
-        name = name.trim();
-        if (name.isEmpty()) throw new IllegalArgumentException("Name can't be empty or null");
-        if (name.length() < 3) throw new IllegalArgumentException("Name length must be at least 3 characters");
-        if (name.length() > 200) throw new IllegalArgumentException("Name length cannot be longer than 200 characters");
+        title = title.trim();
+        if (title.isEmpty()) throw new IllegalArgumentException("Name can't be empty or null");
+        if (title.length() < 3) throw new IllegalArgumentException("Name length must be at least 3 characters");
+        if (title.length() > 200) throw new IllegalArgumentException("Name length cannot be longer than 200 characters");
     }
 
 }
+
+//idea refactorizacion: puedo hacer variables min_length y max_length para no hardcodear, si el dia de mañana quiero cambiar ese maximo solo cambio el valor de la variable
